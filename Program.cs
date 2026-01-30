b85ba12b297a6f527fc846ad435d330b0dd1e969
abdelrahan75 <145610444+abdelrahan75@users.noreply.github.com>
2026-01-30 03:29:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region problem_1
+
+            //try
+            //{
+            //    string s = Console.ReadLine();
+            //    int n1 = int.Parse(s);
+            //    int n2 = Convert.ToInt32(s);
+
+            //    Console.WriteLine($"num is {n1} and {n2}");
+
+            //}
+            //catch (FormatException ) {
+            //    Console.WriteLine("Error: The input is not a valid number.");
+
+            //}
+            #endregion
+
+            #region problem_2
+            //string s = Console.ReadLine();
+            //int x;
+            //bool a = int.TryParse(s, out x);
+            //if (a)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else
+            //{
+            //    Exception exception;
+            //}
+            #endregion
+
+            #region problem_3 
+
+            //     public static void Print(object item)
+            //{
+            //    // print 
+            //    // Developing time
+            //    // Defensive code
+            //    Console.WriteLine(item.GetHashCode()); 
+            //}
+            #endregion
+
+            #region problem_4 
+            ////point p1 = new point();
+            ////point p2 = new point();
+            ////p1.x = 1;
+            ////p1 = p2;
+            ////p2.x = 2;
+            //////Console.WriteLine(p1.x);
+            #endregion
+
+            #region problem_5 
+            //string message = "Hello";
+            //Console.WriteLine("Before modification:");
+            //Console.WriteLine(message);
+            //Console.WriteLine("HashCode: " + message.GetHashCode());
+            //message = message + " Hi Willy";
+            //Console.WriteLine("\nAfter modification:");
+            //Console.WriteLine(message);
+            //Console.WriteLine("HashCode: " + message.GetHashCode());
+            #endregion
+
+            #region problem_6 
+
+            //StringBuilder sp=new StringBuilder("hallo");
+            //Console.WriteLine(sp);
+            //Console.WriteLine(sp.GetHashCode());
+            //sp.Append("abdo");
+            //Console.WriteLine(sp);
+            //Console.WriteLine(sp.GetHashCode());
+            #endregion
+
+            #region problem_7
+
+
+
+        }
+    }
+}
+
+
